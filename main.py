2bba1b3fc418dbc11928198749bc27cf60dc2e7f
Kyeongho <dagh1218@naver.com>
2025-06-22 21:24:37 +0900

    [bitnari] #3 chatgpt를 통한 매매 의견 요청 기능 구현
    - chatgpt 를 통해 현재 시점 기준 30일치의 비트코인 차트 데이터를 입력으로 하여 매매의견 질의응답 처리 구현

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
 import pyupbit
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
 
 df = pyupbit.get_ohlcv('KRW-BTC', count=30, interval='day')
 print(df)
+
+client = OpenAI()
+
+response = client.chat.completions.create(
+    model="gpt-4.1",
+    messages=[
+        {
+            "role": "system",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "You are now a Bitcoin trader, and you will use your well-rounded knowledge to identify whether and why to buy, sell, etc. based on the given chart data in order to make the most profit in the shortest possible time.\n\nThe chart data you receive as input is the opening, high, and closing price of Bitcoin for the previous 30 days, and the response is in the form of JSON as shown below.\n\n{\n\t\"decision\": \"buy\",\n\t\"reason\" : \"some reason\"\n},\n{\n\t\"decision\" : \"sell\",\n\t\"reason\" : \"some reason\"\n},\n{\n\t\"decision\" : \"hold\",\n\t\"reason\" : \"some reason\"\n}"
+                }
+            ]
+        },
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": df.to_json()
+                }
+            ]
+        }
+    ],
+    response_format={
+        "type": "json_object"
+    }
+)
+
+print(response.choices[0].message.content)

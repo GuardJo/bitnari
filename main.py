27c808445e8bbd88a2c1e55b6ba4bd5b090ea14b
Kyeongho <dagh1218@naver.com>
2025-06-23 22:19:29 +0900

    [bitnari] 기존 코드 정리 및 로깅 기능 추가
    - 기능별 별도 함수로 분리
    - 로깅을 통한 데이터 이력 관리

--- main.py
+++ main.py
@@ -1,39 +1,62 @@
+import json
+import logging
+
 import pyupbit
 from dotenv import load_dotenv
 from openai import OpenAI
 
+# 로그 설정
+logging.basicConfig(filename="bitnari.log", level=logging.INFO, format='%(asctime)s | %(levelname)s | %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+# 환경변수 로딩
 load_dotenv()
 
-df = pyupbit.get_ohlcv('KRW-BTC', count=30, interval='day')
-print(df)
 
-client = OpenAI()
+# 업비트 30일봉 차트 조회
+def get_month_charts():
+    return pyupbit.get_ohlcv('KRW-BTC', count=30, interval='day')
 
-response = client.chat.completions.create(
-    model="gpt-4.1",
-    messages=[
-        {
-            "role": "system",
-            "content": [
-                {
-                    "type": "text",
-                    "text": "You are now a Bitcoin trader, and you will use your well-rounded knowledge to identify whether and why to buy, sell, etc. based on the given chart data in order to make the most profit in the shortest possible time.\n\nThe chart data you receive as input is the opening, high, and closing price of Bitcoin for the previous 30 days, and the response is in the form of JSON as shown below.\n\n{\n\t\"decision\": \"buy\",\n\t\"reason\" : \"some reason\"\n},\n{\n\t\"decision\" : \"sell\",\n\t\"reason\" : \"some reason\"\n},\n{\n\t\"decision\" : \"hold\",\n\t\"reason\" : \"some reason\"\n}"
-                }
-            ]
-        },
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "text",
-                    "text": df.to_json()
-                }
-            ]
+
+def get_answer_of_trade_reason(df):
+    # OpenAI 비트코인 매매 의사 질의 요청
+    client = OpenAI()
+    response = client.chat.completions.create(
+        model="gpt-4.1",
+        messages=[
+            {
+                "role": "system",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "You are now a Bitcoin trader, and you will use your well-rounded knowledge to identify whether and why to buy, sell, etc. based on the given chart data in order to make the most profit in the shortest possible time.\n\nThe chart data you receive as input is the opening, high, and closing price of Bitcoin for the previous 30 days, and the response is in the form of JSON as shown below.\n\n{\n\t\"decision\": \"buy\",\n\t\"reason\" : \"some reason\"\n},\n{\n\t\"decision\" : \"sell\",\n\t\"reason\" : \"some reason\"\n},\n{\n\t\"decision\" : \"hold\",\n\t\"reason\" : \"some reason\"\n}"
+                    }
+                ]
+            },
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": df.to_json()
+                    }
+                ]
+            }
+        ],
+        response_format={
+            "type": "json_object"
         }
-    ],
-    response_format={
-        "type": "json_object"
-    }
-)
+    )
 
-print(response.choices[0].message.content)
+    return response.choices[0].message.content
+
+
+def main():
+    df = get_month_charts()
+    aiResult = get_answer_of_trade_reason(df)
+
+    logging.info("Response from OpenAI : %s", aiResult)
+
+    jsonResult = json.loads(aiResult)
+
+
+main()
